f2a5eb49caec2f95045c8667dd3595a316b04a16
Bellamkonda Bharat Kumar <pavinan@users.noreply.github.com>
2018-12-08 16:22:28 +0530

    added unit tests

--- src/test/java/com/github/pavinan/java/devextreme/helpers/datasource/UnitTests.java
+++ src/test/java/com/github/pavinan/java/devextreme/helpers/datasource/UnitTests.java
@@ -3,13 +3,16 @@
  */
 package com.github.pavinan.java.devextreme.helpers.datasource;
 
-import org.antlr.runtime.ANTLRFileStream;
-import org.antlr.v4.parse.ANTLRParser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.tree.Tree;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.awt.List;
 
+import com.github.pavinan.java.CaseChangingCharStream;
+import com.github.pavinan.java.mysqlparser.*;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 
@@ -24,6 +27,55 @@ public class UnitTests {
         parseAndTest(json);
     }
 
+    @Test
+    public void test2() throws Exception {
+
+        String json = "[[\"CustomerID\",\"=\",\"ALFKI\"],\"or\",[\"CustomerID\",\"=\",\"ANATR\"]]";
+
+        parseAndTest(json);
+    }
+
+    @Test
+    public void test3() throws Exception {
+        
+        String json = "[\"!\",[[\"CustomerID\",\"=\",\"ALFKI\"],\"or\",[\"CustomerID\",\"=\",\"ANATR\"]]]";
+
+        parseAndTest(json);
+    }
+
+    @Test
+
+    public void test4() throws Exception {
+
+        String json = "[[\"!\",[[\"CustomerID\",\"=\",\"ALFKI\"],\"or\",[\"CustomerID\",\"=\",\"ANATR\"]]],\"and\",[\"!\",[[\"ShipCountry\",\"=\",\"Argentina\"],\"or\",[\"ShipCountry\",\"=\",\"Austria\"]]]]";
+
+        parseAndTest(json);
+    }
+
+    @Test
+    public void test5() throws Exception {
+
+        String json = "[[\"ShipCountry\",\"contains\",\"i\"],\"and\",[\"!\",[[\"CustomerID\",\"=\",\"ALFKI\"],\"or\",[\"CustomerID\",\"=\",\"ANATR\"]]],\"and\",[\"!\",[[\"ShipCountry\",\"=\",\"Argentina\"],\"or\",[\"ShipCountry\",\"=\",\"Austria\"]]]]";
+
+        parseAndTest(json);
+    }
+
+    @Test
+    public void test6() throws Exception {
+
+        String json = "[[\"ShipCountry\",\"contains\",\"i\"],\"and\",[\"!\",[[\"CustomerID\",\"=\",\"ALFKI\"],\"or\",[\"CustomerID\",\"=\",\"ANATR\"]]],\"and\",[[[\"OrderDate\",\">=\",\"1996-07-08T00:00:00\"],\"and\",[\"OrderDate\",\"<\",\"1996-07-09T00:00:00\"]],\"or\",[[\"OrderDate\",\">=\",\"1996-07-11T00:00:00\"],\"and\",[\"OrderDate\",\"<\",\"1996-07-12T00:00:00\"]],\"or\",[[\"OrderDate\",\">=\",\"1996-07-12T00:00:00\"],\"and\",[\"OrderDate\",\"<\",\"1996-07-13T00:00:00\"]],\"or\",[[\"OrderDate\",\">=\",\"1996-07-15T00:00:00\"],\"and\",[\"OrderDate\",\"<\",\"1996-07-16T00:00:00\"]],\"or\",[[\"OrderDate\",\">=\",\"1996-07-18T00:00:00\"],\"and\",[\"OrderDate\",\"<\",\"1996-07-19T00:00:00\"]],\"or\",[[\"OrderDate\",\">=\",\"1996-07-19T00:00:00\"],\"and\",[\"OrderDate\",\"<\",\"1996-07-20T00:00:00\"]],\"or\",[[\"OrderDate\",\">=\",\"1996-07-26T00:00:00\"],\"and\",[\"OrderDate\",\"<\",\"1996-07-27T00:00:00\"]]],\"and\",[\"!\",[[\"ShipCountry\",\"=\",\"Argentina\"],\"or\",[\"ShipCountry\",\"=\",\"Austria\"]]]]";
+
+        parseAndTest(json);
+    }
+
+    @Test
+    public void test7() throws Exception {
+
+        String json = "[[\"col1\", \"=\", 45], [\"col2\", \"=\", true]]";
+
+        parseAndTest(json);
+    }
+
     private void parseAndTest(String json) throws Exception {
 
         JsonArray jArray = (JsonArray) (new JsonParser().parse(json));
@@ -31,6 +83,29 @@ public class UnitTests {
         System.out.println();
 
         String sql = builder.buildFor(jArray);
-        
+
+        sql = "SELECT * FROM Customers where " + sql + "";
+
+        CharStream charStream = CharStreams.fromString(sql);
+        CaseChangingCharStream upper = new CaseChangingCharStream(charStream, true);
+
+        MySqlLexer lexer = new MySqlLexer(upper);
+
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        MySqlParser parser = new MySqlParser(tokenStream);
+
+        // parser.setErrorHandler(new BailErrorStrategy());
+        parser.setBuildParseTree(false);
+
+        String tree = parser.root().toStringTree();
+
+        System.out.println(tree);
+
+        int totalErrors = parser.getNumberOfSyntaxErrors();
+
+        System.out.println("Total errors : " + totalErrors);
+
+        assertEquals(0, totalErrors);
+
     }
 }
---
+++ src/test/java/com/github/pavinan/java/tsqlparser/MyTsqlListener.java
@@ -0,0 +1,7 @@
+package com.github.pavinan.java.tsqlparser;
+
+import com.github.pavinan.java.tsqlparser.TSqlParser.Dml_clauseContext;
+
+public class MyTsqlListener extends TSqlParserBaseListener {
+    
+}
